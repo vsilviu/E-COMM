ee6e32a0e19f93fcd8ed3b1ff471298750ea967c
Silviu <v@f.c>
2016-01-29 13:47:10 +0200

    final tweaks for first version

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,3 +1,33 @@
 /**
  * Created by Silviu on 1/29/16.
  */
+(function(angular) {
+    var AppController = function($scope, Item) {
+        Item.query(function(response) {
+            $scope.items = response ? response : [];
+        });
+
+        $scope.addItem = function(description) {
+            new Item({
+                description: description,
+                checked: false
+            }).$save(function(item) {
+                $scope.items.push(item);
+            });
+            $scope.newItem = "";
+        };
+
+        $scope.updateItem = function(item) {
+            item.$update();
+        };
+
+        $scope.deleteItem = function(item) {
+            item.$remove(function() {
+                $scope.items.splice($scope.items.indexOf(item), 1);
+            });
+        };
+    };
+
+    AppController.$inject = ['$scope', 'Item'];
+    angular.module("myApp.controllers").controller("AppController", AppController);
+}(angular));
